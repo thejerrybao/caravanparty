e394be4616e3bcd4096bd4b6e0822d4f68f9b61b
Jerry Bao <thejerrybao@gmail.com>
2014-10-24 18:15:47 -0700

    finished iter1 controller functions

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+    # POST /register
+    def register
+        newUser = User.register(params[:username], params[:password])
+        render json: newUser
+    end
+
+    # POST /login
+    def login
+        login = User.login(params[:username], params[:password])
+        render json: login
+    end
+
+    # GET /users/:user_id
+    def show
+        user = User.getUser(params[:id])
+        render json: user
+    end
+
+    # GET /users/:user_id/location
+    def location
+        userLocation = User.getUserLocation(params[:id])
+        render json: userLocation
+    end
+
+    def caravans
+        userCaravans = User.getUserCaravans(params[:id])
+        render json: userCaravans
+    end
+
 end
