cb2a54a1e6d8dd4afa9a8ac278b8073e016b968f
Kevin Chen <calmthatwombat>
2014-10-24 20:03:50 -0700

    finished models and controllers but did not test at all yet

--- server/app/controllers/caravans_controller.rb
+++ server/app/controllers/caravans_controller.rb
@@ -3,18 +3,16 @@ $SUCCESS = 1
 class CaravansController < ApplicationController
   
   # GET caravans/:caravan_id
-  def show()
+  def show
     id = params[:caravan_id]
     caravan = Caravan.get_caravan(id)
-    if caravan.is_a? Integer
+    if caravan.is_a? Integer    # error code
       render json: {reply_code: caravan}
       return
     end
 
-    participant_ids = CaravanUser.where(caravan_id: id, accepted_invitation: true).map{|user| user.user_id}
-    
     render json: {reply_code: $SUCCESS, caravan_id: id, 
-      host_id: caravan.host_user_id, participants: participant_ids}
+      host_id: caravan.host_user_id, participants: Caravan.get_participants(id)}
   end
 
 end
